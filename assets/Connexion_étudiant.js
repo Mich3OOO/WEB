d3b4ee395eabf2f98bc323c9ed0853e4ce1d4346
Mich3OOO <sepulmichael@hotmail.com>
2024-02-23 13:48:35 +0100

    update

--- assets/Connexion_étudiant.js
+++ assets/Connexion_étudiant.js
@@ -3,14 +3,26 @@ let Offres = document.getElementsByClassName("Offre");
 
 for(let i = 0 ; i< Offres.length;i++)
 {
-    Offres[i].children[0].addEventListener("click",ShowOffre);
+    
+    Offres[i].children[0].addEventListener("click",ShowOffre)(Offres[i].id);
+    Offres[i].children[1].addEventListener("click",addToWishList);
 }
 
-
-
 console.log("b");
-function ShowOffre()
+function ShowOffre(id)
 {
-    console.log("a");
+    console.log(id);
+    document.body.insertAdjacentHTML("beforeend","<div id='BGPopup'></div><div id = 'StagePopUp'><button>x</button><div id='infostage'><h2>Nom du poste</h2><p>Résumé stage</p></div><div id='infoEntreprise'><h3>Nom Entreprise</h3><h4>Ville</h4><p>Résumé Entreprise</p></div></div>");
+    document.getElementById("BGPopup").addEventListener("click",closePopup);
+    document.getElementById("StagePopUp").children[0].addEventListener("click",closePopup);
+}
 
+function closePopup()
+{
+    document.getElementById("BGPopup").remove();
+    document.getElementById("StagePopUp").remove();
+}
+function addToWishList()
+{
+    console.log("element");
 }
