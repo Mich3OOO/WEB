7ceea345fc8498d2e2cb7dae7f0d7ab0aed9c676
PersilZ <jarod.meriaux@viacesi.fr>
2024-03-12 14:11:01 +0100

    wishlist ouiiii

--- assets/js/Wishlist.js
+++ assets/js/Wishlist.js
@@ -1,11 +1,14 @@
 let taille = document.getElementsByClassName("plus");
 
 for(i=0;i<taille.length;i++){
+    
     taille[i].addEventListener("click", function(event){
-        if(document.getElementById("ID_"+i) != null){
-            document.getElementById("ID_"+i).remove();
+        let idParent = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if(document.getElementById("ID_"+idParent) != null){
+            document.getElementById("ID_"+idParent).remove();
         } else {
-            event.target.parentNode.parentNode.parentNode.insertAdjacentHTML("beforeend","<div class='plusText' id='ID_"+i+"'><p>Info supplémentaires</p></div>");
+            
+            event.target.parentNode.parentNode.parentNode.insertAdjacentHTML("beforeend","<div class='plusText' id='ID_"+idParent+"'><p>Info supplémentaires</p></div>");
         }
 });
 };
