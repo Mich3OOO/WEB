5711916967a7f5f942f74ab9090c114556a5d334
PersilZ <jarod.meriaux@viacesi.fr>
2024-03-15 15:39:26 +0100

    oui

---
+++ FAQ.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" >
+        <link href="assets/css/FAQ.css" rel="stylesheet" type="text/css" />
+        <script src="assets/js/include.js"></script>
+        <title>F.A.Q</title>
+    </head>
+    <body>
+
+    </body>
+</html>
