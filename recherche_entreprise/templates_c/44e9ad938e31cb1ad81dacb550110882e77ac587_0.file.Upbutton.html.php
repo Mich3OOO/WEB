7601147f27af58a894325abbff9019d14a420f8d
GahikoVON <fabien.64000@outlook.fr>
2024-03-29 10:26:15 +0000

    update

---
+++ recherche_entreprise/templates_c/44e9ad938e31cb1ad81dacb550110882e77ac587_0.file.Upbutton.html.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 4.5.1, created on 2024-03-29 11:18:19
+  from 'C:\Users\fabie\OneDrive - Association Cesi Viacesi mail\CESI\CPI-A2\BLOC 4\PROJET\site web\WEB\assets\html\Upbutton.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.1',
+  'unifunc' => 'content_6606956b62f212_64439001',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '44e9ad938e31cb1ad81dacb550110882e77ac587' => 
+    array (
+      0 => 'C:\\Users\\fabie\\OneDrive - Association Cesi Viacesi mail\\CESI\\CPI-A2\\BLOC 4\\PROJET\\site web\\WEB\\assets\\html\\Upbutton.html',
+      1 => 1711707240,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6606956b62f212_64439001 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<button id = "upbutton">▲</button>
+<link rel="stylesheet" href="./../assets/css/upbutton.css">
+<?php echo '<script'; ?>
+ async src="../assets/js/upbutton.js"><?php echo '</script'; ?>
+><?php }
+}
