dd0b57a21783eca75e954cca96eef654c37d5468
Alejandro Tellez <alexandre.tellez@viacesi.fr>
2024-03-30 18:45:24 +0100

    Cadiz

--- assets/phpScripts/SQL/AjoutStage.php
+++ assets/phpScripts/SQL/AjoutStage.php
@@ -2,8 +2,8 @@
     include "PDO.php";
     $connexion = new Sql(1);
 
-
-    $requête = $connexion->Set("INSERT INTO entreprise (NomE, descr, MailE, TelE, Site) VALUES('".$_POST['Poste']."', '".$_POST['Nom_Entreprise']."', '".$_POST['Renumeration']."', '".$_POST['NB_Places']."');");
-    $requête2 = $connexion->Set("INSERT INTO adresse (adresseA) VALUES ('".$_POST['Adresse']."');");
-    $requête3 = $connexion->Set("INSERT INTO ville (Code_Post, ville) VALUES ('".$_POST['CP']."', '".$_POST['Ville']."');");
+    $requête = $connexion->Set("SELECT IDE from entreprise WHERE NomE = '".$_POST['Nom_Entreprise']."' ");
+    $requête2 = $connexion->Set("INSERT INTO offre (Poste, remune, Nb_place, Description, IDE) VALUES('".$_POST['Poste']."', '".$_POST['Renumeration']."', '".$_POST['NB_Places']."', '".$_POST['Description']."', '"$requête"');");
+    $requête3 = $connexion->Set("INSERT INTO adresse (adresseA) VALUES ('".$_POST['Adresse']."');");
+    $requête4 = $connexion->Set("INSERT INTO ville (Code_Post, ville) VALUES ('".$_POST['CP']."', '".$_POST['Ville']."');");
 ?>
