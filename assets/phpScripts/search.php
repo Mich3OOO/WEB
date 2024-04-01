da2015cf38e32f466d41051b2bfecfa9dc03184d
Mich3OOO <sepulmichael@hotmail.com>
2024-04-01 14:58:50 +0200

    sql querry fix

--- assets/phpScripts/search.php
+++ assets/phpScripts/search.php
@@ -10,7 +10,7 @@ if(isset($_GET["ville"]) and isset($_GET["reg"]) and isset($_GET["Secteur"]) and
 
 
     $setand = false;
-    $sql = "SELECT n1.IDu as IDu ,offre.IDoffre as IDoffre, Poste, Ville, NomE FROM  OFFRE INNER JOIN entreprise ON Offre.IDE = entreprise.IDE INNER JOIN Secteur_d_activité ON Entreprise.IdSec = Secteur_d_activité.IdSec INNER JOIN adresse ON Entreprise.ID_adresse = adresse.ID_adresse INNER JOIN ville ON adresse.idv = ville.idv INNER JOIN reg ON ville.ID_reg = reg.ID_reg LEFT join ((SELECT * from interesser WHERE IDu = ".$_SESSION["IDu"].") as n1) on n1.IDoffre = offre.IDoffre INNER JOIN viser on offre.IDoffre = viser.IDoffre inner join types_de_promotions on types_de_promotions.IDT = viser.IDT";
+    $sql = "SELECT n1.IDu as IDu ,offre.IDoffre as IDoffre, Poste, Ville, NomE FROM  OFFRE INNER JOIN entreprise ON Offre.IDE = entreprise.IDE INNER JOIN secteur_activite ON Entreprise.IdSec = secteur_activite.IdSec INNER JOIN adresse ON Entreprise.ID_adresse = adresse.ID_adresse INNER JOIN ville ON adresse.idv = ville.idv INNER JOIN reg ON ville.ID_reg = reg.ID_reg LEFT join ((SELECT * from interesser WHERE IDu = ".$_SESSION["IDu"].") as n1) on n1.IDoffre = offre.IDoffre INNER JOIN viser on offre.IDoffre = viser.IDoffre inner join types_promotions on types_promotions.IDT = viser.IDT";
     if($_GET["ville"][0] != "" or $_GET["reg"][0] != "" or $_GET["Secteur"][0] != "" or $_GET["Date"] != "" or $_GET["Duree"] != "" or $_GET["Poste"] != "" or $_GET["Prom"][0] !="")
     {
         $sql = $sql . " WHERE" ;
@@ -27,7 +27,7 @@ if(isset($_GET["ville"]) and isset($_GET["reg"]) and isset($_GET["Secteur"]) and
     }
     if($_GET["Secteur"][0] != "")
     {
-        $sql = $sql . addFiltre("Secteur_d_activité.Secteur_d_activité", $_GET["Secteur"],$setand);
+        $sql = $sql . addFiltre("secteur_activite.secteur_activite", $_GET["Secteur"],$setand);
         $setand = true;
     }
     if($_GET["Prom"][0] != "")
@@ -79,7 +79,7 @@ if(isset($_GET["ville"]) and isset($_GET["reg"]) and isset($_GET["Secteur"]) and
 }
 elseif(isset($_GET["ID"]))
 {
-    echo $con->Getjson("SELECT *,COUNT(postuler.IDu) as post FROM  OFFRE INNER JOIN Viser ON OFFRE.IDoffre = Viser.IDoffre INNER JOIN types_de_promotions ON Viser.IDT = types_de_promotions.IDT INNER JOIN entreprise ON Offre.IDE = entreprise.IDE INNER JOIN Secteur_d_activité ON Entreprise.IdSec = Secteur_d_activité.IdSec INNER JOIN adresse ON Entreprise.ID_adresse = adresse.ID_adresse INNER JOIN ville ON adresse.idv = ville.idv INNER JOIN reg ON ville.ID_reg = reg.ID_reg LEFT JOIN postuler on postuler.IDoffre =offre.IDoffre where Offre.IDoffre = " . $_GET["ID"] . ";"); 
+    echo $con->Getjson("SELECT *,COUNT(postuler.IDu) as post FROM  OFFRE INNER JOIN Viser ON OFFRE.IDoffre = Viser.IDoffre INNER JOIN types_promotions ON Viser.IDT = types_promotions.IDT INNER JOIN entreprise ON Offre.IDE = entreprise.IDE INNER JOIN secteur_activite ON Entreprise.IdSec = secteur_activite.IdSec INNER JOIN adresse ON Entreprise.ID_adresse = adresse.ID_adresse INNER JOIN ville ON adresse.idv = ville.idv INNER JOIN reg ON ville.ID_reg = reg.ID_reg LEFT JOIN postuler on postuler.IDoffre =offre.IDoffre where Offre.IDoffre = " . $_GET["ID"] . ";"); 
 }
 else
 {
