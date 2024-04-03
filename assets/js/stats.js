74d6607c1043561bc7a33f5c99e0bd403aa5e3e9
Mich3OOO <sepulmichael@hotmail.com>
2024-04-03 11:06:14 +0200

    stats

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,47 +1,63 @@
-const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yArray = [55, 49, 44, 24, 15];
+var dataGraph;
 
-const data = [{
-  x:xArray,
-  y:yArray,
-  type:"bar",//pie
-  orientation:"v",
-  marker: {color:"rgba(0,0,255,0.6)"}
+var dat = [{
+  values:[55, 49, 44],
+  labels:['Italy', 'France', "Spain"],
+  type: 'pie'//pie
 }];
 
-const layout = {title:"World Wide Wine Production",
-autosize: true
-};
+var lay = {height: 400,
+  width: 400,}
+if (document.readyState !== 'loading') {
+  init();
+}
+window.addEventListener("DOMContentLoaded",init)
 
-/*
-CentresEtudiant:SELECT ville,count(etudiant.IDu) as nb from ville INNER join classe on classe.idv = ville.idv INNER join etudiant on etudiant.IDClasse = classe.IDClasse GROUP by ville;
-PromotionsEtudiant:SELECT Promotion,COUNT(etudiant.IDu) from classe INNER join etudiant on etudiant.IDClasse =classe.IDClasse INNER join promotion on promotion.IDProm = classe.IDProm GROUP by classe.IDprom;
-Competences: SELECT comp, COUNT(IDoffre) as nb from competences INNer JOIN necessite on necessite.IDComp = competences.IDComp GROUP by competences.IDComp;
-Localitee:SELECT COUNT(IDoffre),ville from offre INNER join entreprise on entreprise.IDE = offre.IDE INNER join adresse on entreprise.ID_adresse = adresse.ID_adresse INNER join ville on ville.idv = adresse.idv GROUP by ville.idv;
-TopWhishList: SELECT offre.IDoffre, count(IDu) as nb from offre INNER JOIN interesser on offre.IDoffre = interesser.IDoffre GROUP by offre.IDoffre LIMIT 5;
-PromotionsConcernees: SELECT nom_du_type, COUNT(IDoffre) as nb from types_promotions INNER join viser on viser.IDT = types_promotions.IDT GROUP by types_promotions.IDT;
-DureeStage: SELECT duree ,COUNT(IDoffre) from offre GROUP by duree;
-LocaliteeEnt: SELECT COUNT(IDE),ville from entreprise INNER join adresse on entreprise.ID_adresse = adresse.ID_adresse INNER join ville on ville.idv = adresse.idv GROUP by ville.idv;
-SecteurAct:SELECT secteur_Act ,COUNT(IDE) from secteur_activite INNER join entreprise on entreprise.IdSec = secteur_activite.IdSec GROUP by secteur_Act;
-TopAnnonces: SELECT offre.IDoffre, count(IDu) as nb from offre INNER JOIN postuler on offre.IDoffre = postuler.IDoffre GROUP by offre.IDoffre LIMIT 5;
+function init()
+{
+  fetch("http://localhost/assets/phpScripts/GetStats.php",{method:"GET"}).then(r => r.json()).then(data => {
+
+  console.log(JSON.stringify(data.PromotionsEtudiant.x));
 
 
+  dataGraph =data;  
+
+  
+
+
+  for(let i in data)
+  {
+    
+    if(i=="TopAnnonces" || i=="TopWhishList" )
+    {
+      
+      Plotly.newPlot(i, [{x:data[i].data.x, y:data[i].data.y , type:"bar"}], {title:data[i].Title,autosize: true});
+    }
+    else
+    {
+      Plotly.newPlot(i, [{labels:data[i].data.x, values:data[i].data.y , type:"pie"}], {title:data[i].Title,autosize: true});
+    }
+  }
 
 
 
-*/
+  
+
+
+  });
+}
 
 
 
 
 
-Plotly.newPlot("CentresEtudiant", data, layout);
-Plotly.newPlot("PromotionsEtudiant", data, layout);
-Plotly.newPlot("Competences", data, layout);
-Plotly.newPlot("Localitee", data, layout);
-Plotly.newPlot("TopWhishList", data, layout);
-Plotly.newPlot("PromotionsConcernees", data, layout);
-Plotly.newPlot("DureeStage", data, layout);
-Plotly.newPlot("LocaliteeEnt", data, layout);
-Plotly.newPlot("SecteurAct", data, layout);
-Plotly.newPlot("TopAnnonces", data, layout);
+
+// Plotly.newPlot("PromotionsEtudiant", data, layout);
+// Plotly.newPlot("Competences", data, layout);
+// Plotly.newPlot("Localitee", data, layout);
+// Plotly.newPlot("TopWhishList", data, layout);
+// Plotly.newPlot("PromotionsConcernees", data, layout);
+// Plotly.newPlot("DureeStage", data, layout);
+// Plotly.newPlot("LocaliteeEnt", data, layout);
+// Plotly.newPlot("SecteurAct", data, layout);
+// Plotly.newPlot("TopAnnonces", data, layout);
