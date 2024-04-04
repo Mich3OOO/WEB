940fc756b557af02f22fd8843807321b18eb71b1
GahikoVON <fabien.64000@outlook.fr>
2024-04-04 17:53:52 +0200

    ajout stage

--- assets/phpScripts/SQL/AjoutStage.php
+++ assets/phpScripts/SQL/AjoutStage.php
@@ -6,12 +6,12 @@
     }
     $connexion = new Sql($_SESSION["role"]);
 
-    if($connexion->Set("IF EXIST (SELECT IDE from entreprise WHERE NomE = '".$_POST['Nom_Entreprise']."');")){
-        $requête = $connexion->Set("SELECT IDE from entreprise WHERE NomE = '".$_POST['Nom_Entreprise']."' ");
-        $requête2 = $connexion->Set("INSERT INTO offre (Poste, remune, Nb_place, Descr, IDE) VALUES('".$_POST['Poste']."', '".$_POST['Renumeration']."', '".$_POST['NB_Places']."', '".$_POST['Description']."', '".$requête."');");
-        $requête3 = $connexion->Set("SELECT IDoffre from offre WHERE Poste = '".$_POST['Poste']."' ");
-        $requête4 = $connexion->Set("SELECT IDT from types_de_promotions WHERE Nom_du_Type = '".$_POST['Promotion']."' ");
-        $requête5 = $connexion->Set("INSERT INTO viser (IDoffre, IDT) VALUES('".$requête3."', '".$requête4."');");
+    if($connexion->Set("IF EXIST (SELECT IDE from entreprise WHERE NomE = '".$_GET['NomE']."');")){
+        $requêteIDE = $connexion->Set("SELECT IDE from entreprise WHERE NomE = '".$_GET['NomE']."' ");
+        $requêteOffre = $connexion->Add("INSERT INTO offre (Duree,Poste, remune, Nb_place, Descr, IDE) VALUES('".$_GET['Duree']."','".$_GET['Poste']."', '".$_GET['Renumeration']."', '".$_GET['NB_Places']."', '".$_GET['Description']."', '".$requêteIDE."');");
+        $requêteIDoffre = $connexion->Set("SELECT IDoffre from offre WHERE Poste = '".$_POST['Poste']."' ");
+        $requêteIDT = $connexion->Set("SELECT IDT from types_de_promotions WHERE Nom_du_Type = '".$_POST['TypePromo']."' ");
+        $requête5 = $connexion->Set("INSERT INTO viser (IDoffre, IDT) VALUES('".$requêteIDoffre."', '".$requêteIDT."');");
     } else {
         echo "L'entreprise n'existe pas ! Veuillez d'abord la créer !";
     }
