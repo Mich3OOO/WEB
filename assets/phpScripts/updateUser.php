06ede8c44df7985d67721e7728b27d0d4d1c263e
PersilZ <jarod.meriaux@viacesi.fr>
2024-04-04 19:20:39 +0100

    UPDATE SAUF PROMO et type, AJOUT COMPTE

--- assets/phpScripts/updateUser.php
+++ assets/phpScripts/updateUser.php
@@ -5,33 +5,23 @@ if(!isset($_SESSION))
         session_start();
     }
 $connexion = new Sql($_SESSION["role"]);
-//$connexion->Update("UPDATE utilisateur,promotion,campus,adresse,ville,reg SET MdpU='".$_GET["Mot-de-passe"]."',NomU='".$_GET["Nom"]."',PrenomU='".$_GET["Prenom"]."',Date_NaisU='".$_GET["Date-naissance"]."',MailU='".$_GET["Mail"]."',role='".$_GET["Role"]."',adresseA='".$_GET["Adresse"]."',promotion='".$_GET["Promotion"]."',NomC='".$_GET["Campus"]."',Code_Post='".$_GET["CP"]."',ville='".$_GET["Ville"]."',reg='".$_GET["Region"]."' FROM utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg INNER JOIN promotion ON utilisateur.IDu=promotion.IDu INNER JOIN campus ON promotion.idCentre=campus.idCentre WHERE IDu ='".$_GET["IDu"]."';");
 
-// $stockId=$connexion->GetFirstRow("Select adresse.ID_adresse,adresse.idv,ville.ID_reg,campus.idCentre FROM utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg INNER JOIN promotion ON utilisateur.IDu=promotion.IDu INNER JOIN campus ON promotion.idCentre=campus.idCentre WHERE utilisateur.IDu ='".$_GET["IDu"]."';");
-
-$test = $connexion->GetLazy("SELECT IF( EXISTS ( SELECT ID_adresse FROM adresse INNER JOIN ville ON ville.idv = adresse.idv WHERE ville = '".$_GET['Ville']."' AND adresseA='".$_GET['Adresse']."'),1,0);");
-var_dump($test);
-if ($test == false){
-    $test2 = $connexion->Set("SELECT IF( EXISTS ( SELECT idv FROM ville WHERE ville = '".$_GET['Ville']."'),1,0);");
-    var_dump($test2);
-    if($test2==true){
-        $StockVille = $connexion->GetFirstRow("SELECT idv FROM ville WHERE ville = '".$_GET['Ville']."';");
-        $requeteAdresse = $connexion->add("INSERT INTO Adresse(adresseA, complementA, idv) VALUES ('".$_GET['Adresse']."', '".$_GET['Complement']."', '".$StockVille[0]."')");
-    } else { 
-        $requeteRegion = $connexion->add("INSERT INTO reg(reg) VALUES ('".$_GET['Region']."')");
-        $StockRegion = $connexion->GetFirstRow("SELECT ID_reg FROM reg WHERE reg = '".$requeteRegion[0]."';");
+$test = $connexion->GetFirstRow("SELECT IF( EXISTS ( SELECT ID_adresse FROM adresse INNER JOIN ville ON ville.idv = adresse.idv WHERE ville = '".$_GET['Ville']."' AND adresseA='".$_GET['Adresse']."'),1,0);");
+if ($test[0] == 0){
+    $test2 = $connexion->GetFirstRow("SELECT IF( EXISTS ( SELECT idv FROM ville WHERE ville = '".$_GET['Ville']."'),1,0);");
+    if($test2[0]== 0){
+        $StockRegion = $connexion->GetFirstRow("SELECT ID_reg FROM reg WHERE reg = '".$_GET['Region']."';");
         $requeteVille = $connexion->add("INSERT INTO ville(ville, Code_Post, ID_adresse) VALUES ('".$_GET['Ville']."','".$_GET['CP']."','".$StockRegion[0]."');");
-        $StockVille = $connexion->GetFirstRow("SELECT idv FROM ville WHERE ville = '".$_GET['Ville']."';");
-        $requeteAdresse = $connexion->add("INSERT INTO Adresse(adresseA, complementA, idv) VALUES ('".$_GET['Adresse']."', '".$_GET['Complement']."', '".$StockVille[0]."')");
     }
 }
+$StockVille = $connexion->GetFirstRow("SELECT idv FROM ville WHERE ville = '".$_GET['Ville']."';");
+$requeteAdresse = $connexion->add("INSERT INTO Adresse(adresseA, idv) VALUES ('".$_GET['Adresse']."', '".$StockVille[0]."')");
 $StockAdresse = $connexion->GetFirstRow("SELECT ID_adresse FROM adresse INNER JOIN ville ON ville.idv = adresse.idv WHERE ville = '".$_GET['Ville']."' AND adresseA = '".$_GET['Adresse']."';");    
-var_dump($StockAdresse);
 
 $roleUti = $connexion->GetFirstRow("SELECT role FROM utilisateur WHERE NomU = '".$_GET['Nom']."' AND PrenomU = '".$_GET['Prenom']."';");
 if($roleUti[0] == 'Etudiant'){
     if($_GET["Mot-de-passe"]!=""){
-        $connexion->Update("UPDATE utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg  SET MdpU='".$_GET["Mot-de-passe"]."',
+        $connexion->Update("UPDATE utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg  SET MdpU='".password_hash($_GET["Mot-de-passe"],PASSWORD_DEFAULT)."',
         NomU='".$_GET["Nom"]."',
         PrenomU='".$_GET["Prenom"]."',
         Date_NaisU='".$_GET["Date-naissance"]."',
@@ -50,7 +40,7 @@ if($roleUti[0] == 'Etudiant'){
     }
 } else {
     if($_GET["Mot-de-passe"]!=""){
-        $connexion->Update("UPDATE utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg SET MdpU='".$_GET["Mot-de-passe"]."',
+        $connexion->Update("UPDATE utilisateur INNER JOIN adresse ON utilisateur.ID_adresse=adresse.ID_adresse INNER JOIN ville ON ville.idv=adresse.idv INNER JOIN reg ON reg.ID_reg=ville.ID_reg SET MdpU='".password_hash($_GET["Mot-de-passe"],PASSWORD_DEFAULT)."',
         NomU='".$_GET["Nom"]."',
         PrenomU='".$_GET["Prenom"]."',
         Date_NaisU='".$_GET["Date-naissance"]."',
