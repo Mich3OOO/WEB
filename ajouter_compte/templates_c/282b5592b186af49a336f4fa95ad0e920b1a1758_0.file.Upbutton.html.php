9115f9718eff0a8cc1365f14c09d756727177a72
GahikoVON <fabien.64000@outlook.fr>
2024-04-04 07:55:02 +0000

    delete modif

---
+++ ajouter_compte/templates_c/282b5592b186af49a336f4fa95ad0e920b1a1758_0.file.Upbutton.html.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 4.5.1, created on 2024-04-03 21:22:39
+  from 'C:\Users\Utilisateur\OneDrive - Association Cesi Viacesi mail\CESI\CPI-A2\BLOC 4\PROJET\site web\WEB\assets\html\Upbutton.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.1',
+  'unifunc' => 'content_660dac7f2ef8f0_00426124',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '282b5592b186af49a336f4fa95ad0e920b1a1758' => 
+    array (
+      0 => 'C:\\Users\\Utilisateur\\OneDrive - Association Cesi Viacesi mail\\CESI\\CPI-A2\\BLOC 4\\PROJET\\site web\\WEB\\assets\\html\\Upbutton.html',
+      1 => 1711707240,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_660dac7f2ef8f0_00426124 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<button id = "upbutton">▲</button>
+<link rel="stylesheet" href="./../assets/css/upbutton.css">
+<?php echo '<script'; ?>
+ async src="../assets/js/upbutton.js"><?php echo '</script'; ?>
+><?php }
+}
---
+++ recherche/templates_c/a821dad9c599db6e56b273aed19dce10e2cbab2b_0.file.main.tpl.php
@@ -0,0 +1,67 @@
+<?php
+/* Smarty version 4.5.1, created on 2024-04-03 21:29:04
+  from 'C:\Users\Utilisateur\OneDrive - Association Cesi Viacesi mail\CESI\CPI-A2\BLOC 4\PROJET\site web\WEB\assets\tpl\main.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.1',
+  'unifunc' => 'content_660dae0033bb25_59528603',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a821dad9c599db6e56b273aed19dce10e2cbab2b' => 
+    array (
+      0 => 'C:\\Users\\Utilisateur\\OneDrive - Association Cesi Viacesi mail\\CESI\\CPI-A2\\BLOC 4\\PROJET\\site web\\WEB\\assets\\tpl\\main.tpl',
+      1 => 1712157681,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:../tpl/Menu.tpl' => 1,
+    'file:../html/Footer.html' => 1,
+    'file:../html/Upbutton.html' => 1,
+  ),
+),false)) {
+function content_660dae0033bb25_59528603 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <title><?php echo $_smarty_tpl->tpl_vars['titre']->value;?>
+</title>
+        <meta charset="utf-8" >
+        <meta name="keywords" content="<?php echo $_smarty_tpl->tpl_vars['keywords']->value;?>
+">
+        <meta name="description" content="<?php echo $_smarty_tpl->tpl_vars['description']->value;?>
+">
+
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+        <link href="<?php echo $_smarty_tpl->tpl_vars['dircss']->value;?>
+" rel="stylesheet" type="text/css" />
+        <?php echo '<script'; ?>
+ async src="<?php echo $_smarty_tpl->tpl_vars['dirjs']->value;?>
+"><?php echo '</script'; ?>
+>
+        
+        <link rel="manifest" href="../manifest.json">
+        <?php echo '<script'; ?>
+ src="../assets/js/sw.js"><?php echo '</script'; ?>
+>
+    </head>
+    <body>
+        <?php $_smarty_tpl->_subTemplateRender("file:../tpl/Menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        <?php $_smarty_tpl->_subTemplateRender(((string)$_smarty_tpl->tpl_vars['dirfile']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+
+        <?php $_smarty_tpl->_subTemplateRender("file:../html/Footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        <?php $_smarty_tpl->_subTemplateRender("file:../html/Upbutton.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        
+    </body>
+
+</html><?php }
+}
