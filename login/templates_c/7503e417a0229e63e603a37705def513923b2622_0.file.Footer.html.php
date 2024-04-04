6b054c837e39efe61d716e9a1cb3e92e1ae548e8
GahikoVON <fabien.64000@outlook.fr>
2024-04-04 15:17:27 +0000

    menu, pas touche

---
+++ login/templates_c/7503e417a0229e63e603a37705def513923b2622_0.file.Footer.html.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 4.5.1, created on 2024-04-04 15:42:12
+  from 'C:\Users\fabie\OneDrive - Association Cesi Viacesi mail\CESI\CPI-A2\BLOC 4\PROJET\site web\WEB\assets\html\Footer.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.1',
+  'unifunc' => 'content_660eae34448ff8_44332137',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7503e417a0229e63e603a37705def513923b2622' => 
+    array (
+      0 => 'C:\\Users\\fabie\\OneDrive - Association Cesi Viacesi mail\\CESI\\CPI-A2\\BLOC 4\\PROJET\\site web\\WEB\\assets\\html\\Footer.html',
+      1 => 1711707240,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_660eae34448ff8_44332137 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel = "StyleSheet" href = "../assets/css/footer.css">
+
+<footer>
+    <ul>
+        <li>Nous Contacter </li>
+        <li>Téléphone: 06 09 41 51 00</li>
+        <li>Email: Contact@presquauChaud.com</li>
+        <li>Adresse: 4 Rue de Gaule, Paris 75000</li>
+    </ul>
+</footer>
+            <?php }
+}
