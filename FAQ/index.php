a09641e510d3065bdad8ababb5adf8140495b169
Alejandro Tellez <alexandre.tellez@viacesi.fr>
2024-04-05 09:41:50 +0200

    FAQ

--- FAQ/index.php
+++ FAQ/index.php
@@ -2,7 +2,7 @@
     include "../assets/phpScripts/redirect.php";
 require_once('../smarty/libs/Smarty.class.php');
 $smarty = new Smarty();
-
+        
 $smarty->assign('dirfile', '../html/FAQ.html');
 $smarty->assign('dircss', '../assets/css/FAQ.css');
 $smarty->assign('dirjs', '#');
